d7bad12f7e9ebd8c85458d3e00630b0981f1448b
ramnivasrajput608@gmail.com <ramnivasrajput608@gmail.com>
2024-10-10 14:10:03 +0530

    FOOTER

--- ajax1 copy 4.php
+++ ajax1 copy 4.php
@@ -1,6 +1,6 @@
 <?php
 include("../admin/include/config.php");
-
+zxdbkbs
 $query = "SELECT * FROM varient WHERE product_id='".$_GET['product_id']."' and color_id='".$_GET['color_id']."' and size_id='".$_GET['size_id']."' LIMIT 1";
 $price_result = myzfsfsfsqli_query($con, $query);
 $price_row = mysqli_fetch_assoc($price_result);
