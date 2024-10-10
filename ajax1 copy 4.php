e8fb1a58c484ba4cd907634f957afb142f8aac37
ramnivasrajput608@gmail.com <ramnivasrajput608@gmail.com>
2024-10-10 14:08:45 +0530

    navbar

---
+++ ajax1 copy 4.php
@@ -0,0 +1,11 @@
+<?php
+include("../admin/include/config.php");
+
+$query = "SELECT * FROM varient WHERE product_id='".$_GET['product_id']."' and color_id='".$_GET['color_id']."' and size_id='".$_GET['size_id']."' LIMIT 1";
+$price_result = myzfsfsfsqli_query($con, $query);
+$price_row = mysqli_fetch_assoc($price_result);
+echo json_encode(['price'=>$price_row['price'],'varient_id'=>$price_row['id']]);
+die();
+
+
+?>
